7e5cad1d14653adb0a06abb783e2cb75d366116c
sjorsvartnir <sjorsvartnir@fjorsvartnir>
2016-07-11 15:16:27 -0500

    agregue bootstrap

--- System/Conexion.php
+++ System/Conexion.php
@@ -12,13 +12,13 @@ $user='root';
 $password='';
 $database_name='contactos';
 $query='SELECT DISTINCT c.nomContacto, (SELECT t2.dato FROM iddato t2 WHERE t2.idContacto = t.idContacto and t2.idTipo=1) AS Correo , (SELECT t2.dato FROM iddato t2 WHERE t2.idContacto = t.idContacto and t2.idTipo=2) AS Correo FROM iddato t, idcontacto c Where t.idContacto=c.idContacto';
-$conexion = mysql_connect($server, $user, $password);
+$conexion = mysqli_connect($server, $user, $password);
 if(!$conexion){
     die('No se conecto'.mysql_error());
 }
  //else {
  //   echo 'Te conectaste wiiii';  
-mysql_select_db($database_name,$conexion);
+mysqli_select_db($conexion,$database_name);
 //    if(mysql_query($query,$conexion)){
  //       echo 'Se ejecuto el query';
   //  }
@@ -26,12 +26,12 @@ mysql_select_db($database_name,$conexion);
  //       echo 'No se ejecuto'.mysql_error();
  //   }
 //}
-$peticion=  mysql_query($query);
-while ($row = mysql_fetch_array($peticion)) {
+$peticion=  mysqli_query($query);
+while ($row = mysqli_fetch_array($peticion)) {
     echo '<tr>';
     echo '<td>'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td><td><a>Eliminar</a><br><a>Modificar</a></td>';
     echo '</tr>';
 }
-mysql_close($conexion);
+mysqli_close($conexion);
 ?>
 
